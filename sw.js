511afebd08bc8b983a6f849230fac736dc57beec
wilmeracevedo-dotcom <wilmer.acevedo@gmail.com>
2026-04-13 16:34:23 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
-const CACHE = 'flotafrr-v1';
+const CACHE = 'flotafrr-v2';
+const OLD_CACHES = ['flotafrr-v1'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html'])));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./index.html']))
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => OLD_CACHES.includes(k)).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
